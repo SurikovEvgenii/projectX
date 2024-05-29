0d7440867179d2bbf012088231692d9ff09f05b2
Evgenii Surikov <surikov96@mail.ru>
2024-05-29 15:20:07 +0700

    Correction relation

--- src/main/java/org/projectx/entity/accounts/User.java
+++ src/main/java/org/projectx/entity/accounts/User.java
@@ -1,16 +1,15 @@
 package org.projectx.entity.accounts;
 
 import jakarta.persistence.*;
+import org.projectx.entity.data.Comment;
 
-import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 @Entity
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(length = 20, nullable = false)
     private String login;
@@ -22,70 +21,9 @@ public class User {
     private String number;
     @Column(nullable = false)
     private String email;
-    @OneToMany(mappedBy = "designer", cascade = CascadeType.ALL)
-    private Set<Designer> designerSet = new HashSet<>();
 
-    public User() {
-    }
+    //private Set<Designer> designerSet;
+    @OneToMany(mappedBy = "user")
+    private Set<Comment> commentSet;
 
-    public User(String login, String name, String surname, String number, String email, Set<Designer> designerSet) {
-        this.login = login;
-        this.name = name;
-        this.surname = surname;
-        this.number = number;
-        this.email = email;
-        this.designerSet = designerSet;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Set<Designer> getDesignerSet() {
-        return designerSet;
-    }
-
-    public void setDesignerSet(Set<Designer> designerSet) {
-        this.designerSet = designerSet;
-    }
 }
