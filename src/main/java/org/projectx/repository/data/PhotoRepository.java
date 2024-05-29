b1574eba23e80107be8a046b49f4c65599e4fbe6
Evgenii Surikov <surikov96@mail.ru>
2024-05-29 15:21:00 +0700

    Nothing

--- src/main/java/org/projectx/repository/data/PhotoRepository.java
+++ src/main/java/org/projectx/repository/data/PhotoRepository.java
@@ -1,6 +1,6 @@
-package org.projectx.repository.projects.data;
+package org.projectx.repository.data;
 
-import org.projectx.entity.projects.data.Photo;
+import org.projectx.entity.data.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
