03a2d5219d098839852de486fc0532ee15cc4038
Evgenii Surikov <surikov96@mail.ru>
2024-05-29 15:20:40 +0700

    Correct relation

--- src/main/java/org/projectx/entity/projects/Project.java
+++ src/main/java/org/projectx/entity/projects/Project.java
@@ -1,10 +1,10 @@
 package org.projectx.entity.projects;
 
 import jakarta.persistence.*;
-import org.projectx.entity.projects.data.Comment;
-import org.projectx.entity.projects.data.Photo;
+import org.projectx.entity.accounts.Designer;
+import org.projectx.entity.data.Comment;
+import org.projectx.entity.data.Photo;
 
-import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -19,83 +19,13 @@ public class Project {
     private String description;
     @Column(length = 200)
     private String shortDescription;
-    @OneToMany(mappedBy = "photo", cascade = CascadeType.ALL)
-    private Set<Photo> projectSet;
-    @OneToMany(mappedBy = "comment", cascade = CascadeType.ALL)
-    private Set<Comment> commentSet;
+    @Column
     private Long rating;
-    private Long designerId;
+    @OneToMany(mappedBy = "project")
+    private Set<Photo> photoSet;
+    @OneToMany(mappedBy = "project")
+    private Set<Comment> commentSet;
+    @ManyToOne
+    private Designer designerId;
 
-    public Project() {
-    }
-
-    public Project(String header, String description, String shortDescription, Set<Photo> projectSet, Set<Comment> commentSet, Long rating, Long designerId) {
-        this.header = header;
-        this.description = description;
-        this.shortDescription = shortDescription;
-        this.projectSet = projectSet;
-        this.commentSet = commentSet;
-        this.rating = rating;
-        this.designerId = designerId;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getHeader() {
-        return header;
-    }
-
-    public void setHeader(String header) {
-        this.header = header;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getShortDescription() {
-        return shortDescription;
-    }
-
-    public void setShortDescription(String shortDescription) {
-        this.shortDescription = shortDescription;
-    }
-
-    public Set<Photo> getProjectSet() {
-        return projectSet;
-    }
-
-    public void setProjectSet(Set<Photo> projectSet) {
-        this.projectSet = projectSet;
-    }
-
-    public Set<Comment> getCommentSet() {
-        return commentSet;
-    }
-
-    public void setCommentSet(Set<Comment> commentSet) {
-        this.commentSet = commentSet;
-    }
-
-    public Long getRating() {
-        return rating;
-    }
-
-    public void setRating(Long rating) {
-        this.rating = rating;
-    }
-
-    public Long getDesignerId() {
-        return designerId;
-    }
-
-    public void setDesignerId(Long designerId) {
-        this.designerId = designerId;
-    }
 }
