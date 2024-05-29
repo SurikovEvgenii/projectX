ba559bd4a9b2172f3b89108772b594db6fd388fa
Evgenii Surikov <surikov96@mail.ru>
2024-05-29 15:21:47 +0700

    Correct code

--- src/main/java/org/projectx/repository/data/CommentRepository.java
+++ src/main/java/org/projectx/repository/data/CommentRepository.java
@@ -1,6 +1,6 @@
-package org.projectx.repository.projects.data;
+package org.projectx.repository.data;
 
-import org.projectx.entity.projects.data.Comment;
+import org.projectx.entity.data.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
