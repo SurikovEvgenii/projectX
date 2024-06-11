404332ec87cff00dd89d17b27f35e8e51e7c90f1
Evgenii Surikov <surikov96@mail.ru>
2024-06-11 18:07:59 +0700

    Add lombok

--- src/main/java/org/projectx/entity/data/Photo.java
+++ src/main/java/org/projectx/entity/data/Photo.java
@@ -1,19 +1,25 @@
 package org.projectx.entity.data;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import org.projectx.entity.projects.Project;
 
-import java.util.List;
-import java.util.Objects;
+/**
+ *
+ */
 
+@Data
 @Entity
+@Table(name="photos")
 public class Photo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
     private String url;
+
     @ManyToOne
     private Project project;
-
 }
