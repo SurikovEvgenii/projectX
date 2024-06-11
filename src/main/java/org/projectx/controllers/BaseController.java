b842ced058b5bcc8888904bf7cf47929470cb2ba
Evgenii Surikov <surikov96@mail.ru>
2024-06-11 18:04:08 +0700

    Add mapping

--- src/main/java/org/projectx/controllers/BaseController.java
+++ src/main/java/org/projectx/controllers/BaseController.java
@@ -1,13 +1,37 @@
 package org.projectx.controllers;
 
+import org.projectx.services.account.DesignerRepositoryService;
+import org.projectx.services.projects.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @Controller
 public class BaseController {
 
+    @Autowired
+    ProjectService projectService;
+
+    @Autowired
+    DesignerRepositoryService designerRepositoryService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("project", projectService.findAll());
         return "index";
+    }
+
+    @GetMapping("/registration")
+    public String registration(){
+        return "registration";
+    }
+
+    @GetMapping("/designer/{id}")
+    public String designerPage(@PathVariable Long id, Model model){
+        model.addAttribute("designer", designerRepositoryService.findById(id));
+        return "designer";
     }
 }
