d14debf69c310d635a43f3444833a7fe81beac68
Evgenii Surikov <surikov96@mail.ru>
2024-06-11 18:05:05 +0700

    Add annotation @Repository

--- src/main/java/org/projectx/repository/data/CommentRepository.java
+++ src/main/java/org/projectx/repository/data/CommentRepository.java
@@ -2,6 +2,8 @@ package org.projectx.repository.data;
 
 import org.projectx.entity.data.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
