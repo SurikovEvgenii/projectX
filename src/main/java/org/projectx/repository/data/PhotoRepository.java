2df2d0f0c78789d4cacb1abf29bdf54e9afd8f9c
Evgenii Surikov <surikov96@mail.ru>
2024-06-11 18:08:17 +0700

    Add annotation @Repository

--- src/main/java/org/projectx/repository/data/PhotoRepository.java
+++ src/main/java/org/projectx/repository/data/PhotoRepository.java
@@ -2,6 +2,8 @@ package org.projectx.repository.data;
 
 import org.projectx.entity.data.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
 }
