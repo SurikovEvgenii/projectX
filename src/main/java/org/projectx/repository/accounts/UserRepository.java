3afa3a99fb4a261a46f2c2488456acf3d82b68e9
Evgenii Surikov <surikov96@mail.ru>
2024-06-11 18:12:06 +0700

    Add annotation @Repository

--- src/main/java/org/projectx/repository/accounts/UserRepository.java
+++ src/main/java/org/projectx/repository/accounts/UserRepository.java
@@ -1,9 +1,9 @@
 package org.projectx.repository.accounts;
 
-import org.projectx.entity.accounts.User;
+import org.projectx.entity.accounts.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<UserAccount, Long> {
 }
